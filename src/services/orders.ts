2fc8925e3ae5be6115367d86c05f9f78417746ba
Anou pro <anou.webu@gmail.com>
2026-08-14 19:40:09 +0200

    ✨ feat: add createOrder service with orderItems

--- src/services/orders.ts
+++ src/services/orders.ts
@@ -2,16 +2,26 @@ import { db } from "@/db";
 import { orderItems, orders } from "@/db/schema";
 import { getCurrentUser } from "@/lib/auth-utils";
 
-export async function createOrder(orderItem: {
-    productId: string;
-    quantity: number
-}[]) {
-    const userConnected = await getCurrentUser()
+export async function createOrder(
+  items: { productId: string; quantity: number }[],
+) {
+  const userConnected = await getCurrentUser();
 
-    if(!userConnected) {
-        throw new Error("you are not allowed to create an order")
-    }
-    const [newOrder] = await db.insert(orders).values({userId: userConnected.id}).returning()
-    return newOrder
-    
+  if (!userConnected) {
+    throw new Error("you are not allowed to create an order");
+  }
+
+  const [newOrder] = await db
+    .insert(orders)
+    .values({ userId: userConnected.id })
+    .returning();
+
+  const itemsToInsert = items.map((item) => ({
+    orderId: newOrder.orderId,
+    productId: item.productId,
+    quantity: item.quantity,
+  }));
+
+  return await db.insert(orderItems).values(itemsToInsert).returning()
+
 }
