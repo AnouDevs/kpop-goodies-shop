34da7a9910ee7ef080a843f10f120e637088a602
Anou pro <anou.webu@gmail.com>
2026-08-16 16:57:06 +0200

    ✅ test: add updateProduct RBAC tests

--- src/services/products.test.ts
+++ src/services/products.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from "vitest";
-import { createProduct } from "./products";
+import { createProduct, updateProduct } from "./products";
 import { auth } from "@/lib/auth";
 
 vi.mock("next/headers", () => ({
@@ -22,16 +22,40 @@ const fakeProduct = {
   stock: 5,
 };
 
+const fakeProductId = "123qsdfsd";
+
 describe("createProduct", () => {
   it("throws an error when you are not connected", async () => {
     vi.mocked(auth.api.getSession).mockResolvedValue(null);
-    await expect(createProduct(fakeProduct)).rejects.toThrow("Not authenticated");
+    await expect(createProduct(fakeProduct)).rejects.toThrow(
+      "Not authenticated",
+    );
   });
 
   it("throws an error when user is not admin", async () => {
     vi.mocked(auth.api.getSession).mockResolvedValue({
       user: { role: "client" },
     } as any);
-    await expect(createProduct(fakeProduct)).rejects.toThrow("Insufficient permissions");
+    await expect(createProduct(fakeProduct)).rejects.toThrow(
+      "Insufficient permissions",
+    );
+  });
+});
+
+describe("updateProduct", () => {
+  it("throws an error when you are not connected", async () => {
+    vi.mocked(auth.api.getSession).mockResolvedValue(null);
+    await expect(updateProduct(fakeProductId, fakeProduct)).rejects.toThrow(
+      "Not authenticated",
+    );
+  });
+
+  it("throws an error when user is not admin", async () => {
+    vi.mocked(auth.api.getSession).mockResolvedValue({
+      user: { role: "client" },
+    } as any);
+    await expect(updateProduct(fakeProductId, fakeProduct)).rejects.toThrow(
+      "Insufficient permissions",
+    );
   });
 });
